e9e45c86d86702db8c2d3f06e7a7a71980676a36
DLucero <musicplayer2004@gmail.com>
2024-11-16 11:57:52 +0400

    Basic World Selector

---
+++ Assets/Scripts/World Select.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WorldSelect : MonoBehaviour
+{
+    //Copy This Script for each place and connect the new named button.
+    public void LoadNorthAfricaLevel() //Change North Africa For whatever place needed.
+    {
+        SceneManager.LoadScene("North Africa");
+    }
+}
